c352f73fc0a9e4ebf8a39f6931da9a91c8942e6a
DESKTOP-Q1RB5SU\ljwoo <ljwoon94@gmail.com>
2021-07-05 13:20:14 +0900

    하이퍼레저 패브릭 raft-fabric 오더러 5개 조직 5개

---
+++ web-app/server/printKeyStore.sh
@@ -0,0 +1,19 @@
+echo 'printing keystore for Org1'
+
+ls ../../first-network/crypto-config/peerOrganizations/org1.example.com/users/Admin\@org1.example.com/msp/keystore/
+
+echo 'printing keystore for Org2'
+
+ls ../../first-network/crypto-config/peerOrganizations/org2.example.com/users/Admin\@org2.example.com/msp/keystore/
+
+echo 'printing keystore for Org3'
+
+ls ../../first-network/crypto-config/peerOrganizations/org3.example.com/users/Admin\@org3.example.com/msp/keystore/
+
+echo 'printing keystore for Org4'
+
+ls ../../first-network/crypto-config/peerOrganizations/org4.example.com/users/Admin\@org4.example.com/msp/keystore/
+
+echo 'printing keystore for Org5'
+
+ls ../../first-network/crypto-config/peerOrganizations/org5.example.com/users/Admin\@org5.example.com/msp/keystore/
---
+++ web-app/server/updateKeystore.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo 'printing keystore for Org1'
+
+ORG_1_KEYSTORE=$(ls ../../first-network/crypto-config/peerOrganizations/org1.example.com/users/Admin\@org1.example.com/msp/keystore/)
+ORG_2_KEYSTORE=$(ls ../../first-network/crypto-config/peerOrganizations/org2.example.com/users/Admin\@org2.example.com/msp/keystore/)
+ORG_3_KEYSTORE=$(ls ../../first-network/crypto-config/peerOrganizations/org3.example.com/users/Admin\@org3.example.com/msp/keystore/)
+ORG_4_KEYSTORE=$(ls ../../first-network/crypto-config/peerOrganizations/org4.example.com/users/Admin\@org4.example.com/msp/keystore/)
+ORG_5_KEYSTORE=$(ls ../../first-network/crypto-config/peerOrganizations/org5.example.com/users/Admin\@org5.example.com/msp/keystore/)
+
+ORG_1_PATH_TO_KEYSTORE="Admin@org1.example.com/msp/keystore/"
+ORG_2_PATH_TO_KEYSTORE="Admin@org2.example.com/msp/keystore/"
+ORG_3_PATH_TO_KEYSTORE="Admin@org3.example.com/msp/keystore/"
+ORG_4_PATH_TO_KEYSTORE="Admin@org4.example.com/msp/keystore/"
+ORG_5_PATH_TO_KEYSTORE="Admin@org5.example.com/msp/keystore/"
+
+
+UPDATED_KEYSTORE_ORG_1="$ORG_1_PATH_TO_KEYSTORE$ORG_1_KEYSTORE"
+UPDATED_KEYSTORE_ORG_2="$ORG_2_PATH_TO_KEYSTORE$ORG_2_KEYSTORE"
+UPDATED_KEYSTORE_ORG_3="$ORG_3_PATH_TO_KEYSTORE$ORG_3_KEYSTORE"
+UPDATED_KEYSTORE_ORG_4="$ORG_4_PATH_TO_KEYSTORE$ORG_4_KEYSTORE"
+UPDATED_KEYSTORE_ORG_5="$ORG_5_PATH_TO_KEYSTORE$ORG_5_KEYSTORE"
+
+
+
+echo $UPDATED_KEYSTORE_ORG_1
+
+# sed -i "s|keystore/.*|${UPDATED_KEYSTORE}|g" connection.yaml
+# .* is regex-ese for "any character followed by zero or more of any character(s)"
+
+echo 'updating connection.yaml Org1 adminPrivateKey path with' ${UPDATED_KEYSTORE_ORG_1}
+
+sed -i -e "s|Admin@org1.example.com/msp/keystore/.*|$UPDATED_KEYSTORE_ORG_1|g" connection.yaml
+
+echo 'updating connection.yaml Org2 adminPrivateKey path with' ${UPDATED_KEYSTORE_ORG_2}
+
+sed -i -e "s|Admin@org2.example.com/msp/keystore/.*|$UPDATED_KEYSTORE_ORG_2|g" connection.yaml
+
+echo 'updating connection.yaml Org3 adminPrivateKey path with' ${UPDATED_KEYSTORE_ORG_3}
+
+sed -i -e "s|Admin@org3.example.com/msp/keystore/.*|$UPDATED_KEYSTORE_ORG_3|g" connection.yaml
+
+echo 'updating connection.yaml Org4 adminPrivateKey path with' ${UPDATED_KEYSTORE_ORG_4}
+
+sed -i -e "s|Admin@org4.example.com/msp/keystore/.*|$UPDATED_KEYSTORE_ORG_4|g" connection.yaml
+
+echo 'updating connection.yaml Org5 adminPrivateKey path with' ${UPDATED_KEYSTORE_ORG_5}
+
+sed -i -e "s|Admin@org5.example.com/msp/keystore/.*|$UPDATED_KEYSTORE_ORG_5|g" connection.yaml
